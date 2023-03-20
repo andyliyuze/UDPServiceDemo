a644f1239381002d9da90715537396cfd146583f
liyuze <yuze.li@ehang.com>
2023-03-20 18:08:28 +0800

    添加项目文件。

---
+++ UDPServiceDemo/Program - 复制.cs
@@ -0,0 +1,94 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPServer
+{
+    //Select
+    public static void Main2(string[] args)
+    {
+        // 创建一个 UDP Socket
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        // 绑定本地 IP 地址和端口号
+        IPEndPoint localEP = new IPEndPoint(IPAddress.Any, 12345);
+        socket.Bind(localEP);
+
+        // 设置 Select 方法需要监听的 Socket 集合
+        var checkRead = new[] { socket };
+
+        // 循环监听
+        while (true)
+        {
+            // 使用 Select 方法监听可读状态的 Socket
+            Socket.Select(checkRead, null, null, 1000);
+
+            // 从收到数据的 Socket 中读取数据
+            byte[] buffer = new byte[1024];
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            int length = socket.ReceiveFrom(buffer, ref remoteEP);
+
+            // 处理接收到的数据
+            string data = Encoding.ASCII.GetString(buffer, 0, length);
+            Console.WriteLine("Received: {0} from {1}", data, remoteEP.ToString());
+        }
+    }
+
+    //Poll
+    public static void Main3(string[] args)
+    {
+        // 创建一个 UDP Socket
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        // 绑定本地 IP 地址和端口号
+        IPEndPoint localEP = new IPEndPoint(IPAddress.Any, 12345);
+        socket.Bind(localEP);
+
+        // 循环监听
+        while (true)
+        {
+            // 使用 Poll 方法监听可读状态的 Socket
+            if (socket.Poll(1000, SelectMode.SelectRead))
+            {
+                // 从收到数据的 Socket 中读取数据
+                byte[] buffer = new byte[1024];
+                EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                int length = socket.ReceiveFrom(buffer, ref remoteEP);
+
+                // 处理接收到的数据
+                string data = Encoding.ASCII.GetString(buffer, 0, length);
+                Console.WriteLine("Received: {0} from {1}", data, remoteEP.ToString());
+            }
+        }
+    }
+
+    //
+    public static void Main4(string[] args)
+    {
+        // 创建一个 Socket 对象
+        var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        serverSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+        // 绑定套接字到本地终结点
+        var localEndPoint = new IPEndPoint(IPAddress.Any, 12345);
+        serverSocket.Bind(localEndPoint);
+
+        Console.WriteLine("Server started.");
+
+        while (true)
+        {
+            // 使用 Select 方法等待套接字的可读事件
+            var readList = new List<Socket>() { serverSocket };
+            Socket.Select(readList, null, null, 10000);
+            if (readList.Any())
+            {
+                foreach (var item in readList)
+                {
+                    byte[] buffer = new byte[1024];
+                    EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                    int length = item.ReceiveFrom(buffer, ref remoteEP);
+                }
+            }
+        }
+    }
+}
---
+++ UDPServiceDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    private const int BufferSize = 1024;
+
+    public static void Main(string[] args2)
+    {
+        UDPServer.Main4(args2);
+
+        var endpoint = new IPEndPoint(IPAddress.Any, 12345);
+        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        socket.Bind(endpoint);
+
+        var buffer = new byte[BufferSize];
+        var pool = new SocketAsyncEventArgsPool();
+        for (var i = 0; i < Environment.ProcessorCount * 2; i++)
+        {
+            var args = new SocketAsyncEventArgs();
+            args.SetBuffer(new byte[BufferSize], 0, BufferSize);
+            args.Completed += ReceiveCompleted;
+            args.UserToken = pool;
+            args.RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            pool.Push(args);
+        }
+
+        while (true)
+        {
+            var args = pool.Pop();
+            if (args == null)
+            {
+                Task.Delay(10).Wait();
+                continue;
+            }
+            try
+            {
+                socket.ReceiveFromAsync(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error receiving data: {ex.Message}");
+                pool.Push(args);
+            }
+        }
+    }
+
+    private static void ReceiveCompleted(object sender, SocketAsyncEventArgs args)
+    {
+        Console.WriteLine("RemoteEndPoint:" + args?.RemoteEndPoint?.ToString());
+        if (args.SocketError != SocketError.Success)
+        {
+            Console.WriteLine($"Error receiving data: {args.SocketError}");
+        }
+        else
+        {
+            var data = Encoding.ASCII.GetString(args.Buffer, args.Offset, args.BytesTransferred);
+            Console.WriteLine($"Received data: {data}");
+        }
+
+        var pool = args.UserToken as SocketAsyncEventArgsPool;
+
+        pool.Push(args);
+
+    }
+}
+
+public class SocketAsyncEventArgsPool
+{
+    private readonly object _lock = new object();
+    private readonly Stack<SocketAsyncEventArgs> _pool = new Stack<SocketAsyncEventArgs>();
+
+    public int Count => _pool.Count;
+
+    public void Push(SocketAsyncEventArgs item)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+        lock (_lock) _pool.Push(item);
+    }
+
+    public SocketAsyncEventArgs Pop()
+    {
+        lock (_lock)
+        {
+            return _pool.Count > 0 ? _pool.Pop() : null;
+        }
+    }
+}
